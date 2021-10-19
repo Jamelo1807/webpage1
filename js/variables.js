72a50132ca2a6d0168de77def495659bc3f85075
Jamelo1807 <92825763+Jamelo1807@users.noreply.github.com>
2021-10-19 16:01:59 -0500

    Add files via upload

---
+++ js/variables.js
@@ -0,0 +1,37 @@
+alert(texto);
+
+//Numeros 
+
+var numero = 5;
+var decimal =10.5;
+
+//Cadenas de texto 
+
+var texto = 'bienvenido';
+
+//Booleanos
+
+var booleano1 = true;
+var booleano2 = false;
+
+//Elementos HTML
+var primeraSeccion = document.querySelector('#proyectos');
+var segundaSeccion = document.querySelector('#acerca');
+
+//Arreglos 
+
+var familia = ['Pedro', 'Pascual', numero, true];
+
+//Objetos
+
+var planta = {
+	color: 'verde', tamaño: 'mediano', cantidad: 1
+
+};
+
+//Funciones 
+
+var miFuncion = function(){};
+
+
+
